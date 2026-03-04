6fe64086ef4ac6dafca5a60ad978ffdbaec5e771
TomLiou <tomliou7587@gmail.com>
2026-03-04 20:11:27 +0800

    刪除 trustHost: true

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 const handler = NextAuth({
-  trustHost: true, // 部署到 Vercel 等平台時需要，否則 Google 回調可能失敗
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
